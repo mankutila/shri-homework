ae4570efef3b8f93afdd55d776c84cf4846e3ad0
Maryja Peredryj <chysapko@gmail.com>
2018-04-27 17:18:53 +0300

    Gemini tests setup complete

--- .gemini.js
+++ .gemini.js
@@ -7,12 +7,30 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome',
       }
+    },
+    'mobile-chrome': {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          mobileEmulation: {
+            deviceMetrics: {
+              width: 411,
+              height: 731
+            },
+            userAgent: 'Mozilla/5.0 (Linux; Android 6.0; vivo 1713 Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36'
+          }
+        }
+      }
     }
   },
   sets: {
     desktop: {
       files: 'src/gemini/',
       browsers: ['chrome']
+    },
+    mobile: {
+      files: 'src/gemini/',
+      browsers: ['mobile-chrome']
     }
   }
 };
